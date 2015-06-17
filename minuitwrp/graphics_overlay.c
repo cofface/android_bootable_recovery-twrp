omnirom/android_bootable_recovery

minuitwrp: automatically determine the pixel format.
Change-Id: Ieef5c4d0d15bc103c135d8e63e6bf9a3aead1eec

minuitwrp/graphics_overlay.c
@@ -70,15 +70,27 @@ static int overlayR_id = MSMFB_NEW_REQUEST;
 
 static memInfo mem_info;
 
+static int mdp_pixel_format = -1;
 static int map_mdp_pixel_format()
 {
-    int format = MDP_RGB_565;
-#if defined(RECOVERY_BGRA)
-    format = MDP_BGRA_8888;
-#elif defined(RECOVERY_RGBX)
-    format = MDP_RGBA_8888;
-#endif
-    return format;
+    if(mdp_pixel_format > -1) return mdp_pixel_format;
+    int pixel_format = gr_get_pixel_format();
+    switch(pixel_format)
+    {
+       case GGL_PIXEL_FORMAT_RGBA_8888:
+          mdp_pixel_format = MDP_RGBA_8888;
+          break;
+       case GGL_PIXEL_FORMAT_BGRA_8888:
+          mdp_pixel_format = MDP_BGRA_8888;
+          break;
+       case GGL_PIXEL_FORMAT_RGBX_8888:
+          mdp_pixel_format = MDP_RGBX_8888;
+          break;
+       default:
+          mdp_pixel_format = MDP_RGB_565;
+          break;
+    }
+    return mdp_pixel_format;
 }
 
 static bool overlay_supported = false;

cofface/android_bootable_recovery-twrp

Conflicts:
	minuitwrp/Android.mk
	minuitwrp/graphics.c

minuitwrp/graphics_overlay.c
@@ -69,15 +69,26 @@ static int overlayL_id = MSMFB_NEW_REQUEST;
 static int overlayR_id = MSMFB_NEW_REQUEST;
 
 static memInfo mem_info;
+extern int PIXEL_FORMAT;
 
 static int map_mdp_pixel_format()
 {
-    int format = MDP_RGB_565;
-#if defined(RECOVERY_BGRA)
-    format = MDP_BGRA_8888;
-#elif defined(RECOVERY_RGBX)
-    format = MDP_RGBA_8888;
-#endif
+    int format;
+    switch(PIXEL_FORMAT)
+    {
+       case GGL_PIXEL_FORMAT_RGBA_8888:
+          format = MDP_RGBA_8888;
+          break;
+       case GGL_PIXEL_FORMAT_BGRA_8888:
+          format = MDP_BGRA_8888;
+          break;
+       case GGL_PIXEL_FORMAT_RGBX_8888:
+          format = MDP_RGBX_8888;
+          break;
+       default:
+          format = MDP_RGB_565;
+          break;
+    }
     return format;
 }
 