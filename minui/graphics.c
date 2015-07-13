39556265b2244c61790f32b276350ceab2717f1e
maxwen <max.weninger@gmail.com>
2015-07-14 00:41:14 +0200

    minui: add BOARD_USE_CUSTOM_RECOVERY_FONT
    we want to use it in charger
    
    Change-Id: I3e0288b074bd7203214d218aaee69cb4798e9531

--- minui/graphics.c
+++ minui/graphics.c
@@ -31,7 +31,12 @@
 
 #include <pixelflinger/pixelflinger.h>
 
+#ifdef BOARD_USE_CUSTOM_RECOVERY_FONT
+#include BOARD_USE_CUSTOM_RECOVERY_FONT
+#else
 #include "font_10x18.h"
+#endif
+
 #include "minui.h"
 
 #define NUM_BUFFERS 2
